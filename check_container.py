248a6dfeac64677ea730b4f6d2c253aed04cf1b5
Maneesh Mehra <mmehra@redhat.com>
2024-05-22 16:33:04 -0400

    another

--- check_container.py
+++ check_container.py
@@ -1,6 +1,8 @@
+import sys
 
 def main():
-    print("Checking Container Image")
+    print("Writing to Stdout: Checking Container Image")
+    sys.stderr.write("Writing to StdErr: This is an error message")
 
 if __name__ == "__main__":
     main()
