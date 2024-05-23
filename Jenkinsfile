d3e5ddf728d1f57cd6bdbf2c10b7d2474c881a70
Maneesh Mehra <mmehra@redhat.com>
2024-05-22 22:33:50 -0400

    another

--- Jenkinsfile
+++ Jenkinsfile
@@ -30,13 +30,22 @@ pipeline {
 }
 
 def sourceContainerComplianceCheck() {
-    echo "Running Compliance Check"
-    def status = sh(returnStatus: true, script: """python check_container.py > output.txt 2>&1""")
-    def output = readFile "output.txt"
-    if (status == 0) {
-        echo "The script executed successfully !"
-    } else {
-        echo "The script did not execute successfully !"
-    }
-    echo "Script Output: ${output}"
+
+    def maxRetries = 3
+    def attempts = 0
+    def retry = true
+
+    while (retry && attempts < maxRetries) {
+        echo "Running Compliance Check (Attempt: ${attempts} of ${maxRetries})"
+        def status = sh(returnStatus: true, script: """python check_container.py > output.txt 2>&1""")
+        def output = readFile "output.txt"
+        if (status == 0) {
+            echo "The script executed successfully !"
+            retry = false
+        } else {
+            echo "The script did not execute successfully !"
+            attempts += 1
+        }
+        echo "Script Output: ${output}"
+     }
 }
--- check_container.py
+++ check_container.py
@@ -4,13 +4,21 @@ import random
 def main():
     sys.stdout.write("Writing to StdOut: Checking Container Image\n")
     
-    num = random.randint(1, 1000)
+    number = random.randint(1, 1000)
     
-    if (num % 2 != 0):
+    if (number % 2 != 0):
         sys.stderr.write("Writing to StdErr: This is an error message\n")
-        raise Exception("Raising Exception Due To Number: " + str(num))
+        raise Exception("Raising Exception Due To Number: " + str(number))
     else:
-        sys.stdout.write("No exceptions were raised for Number: " + str(num) + "\n")
+        sys.stdout.write("No exceptions were raised for Number: " + str(number) + "\n")
+        
+        anotherNum = random.randint(1, 1000)
+        if (anotherNum % 2 != 0):
+            sys.stdout.write("ERROR: The Test Failed Since Another Number is: " 
+                             + str(anotherNum) + "\n")
+        else:
+            sys.stdout.write("PASS: The Test Passed Since Another Number is: " 
+                             + str(anotherNum) + "\n")   
         
 if __name__ == "__main__":
     main()
